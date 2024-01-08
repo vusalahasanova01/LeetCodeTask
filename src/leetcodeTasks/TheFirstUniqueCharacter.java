a64330680768a2abeb2a6fcb2c65a3cd78658da9
Vusala Hasanova <vusale.hasanova@caspiantech.az>
2024-01-09 01:47:29 +0400

    https://leetcode.com/problems/first-unique-character-in-a-string/submissions/, 40 ms

---
+++ src/leetcodeTasks/TheFirstUniqueCharacter.java
@@ -0,0 +1,38 @@
+package leetcodeTasks;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class TheFirstUniqueCharacter {
+    public static void main(String[] args) {
+        String s = "dabbcb";
+        int i = firstUniqChar(s);
+        System.out.println(i);
+
+    }
+
+    public static int firstUniqChar(String s) {
+        Hashtable<Character, Integer> countOfChar = new Hashtable<>();
+        for (char c : s.toCharArray()) {
+            if (!countOfChar.containsKey(c)) {
+                countOfChar.put(c, 1);
+            } else {
+                Integer count = countOfChar.get(c);
+                count = count + 1;
+                countOfChar.put(c, count);
+            }
+        }
+
+        int answer = -1;
+        for (Map.Entry<Character, Integer> entry : countOfChar.entrySet()) {
+            Character key = entry.getKey();
+            Integer result = entry.getValue();
+            if (result == 1 && (s.indexOf(key) < answer || answer == -1)) {
+                answer = s.indexOf(key);
+            }
+
+        }
+        return answer;
+    }
+}
